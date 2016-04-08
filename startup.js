6316601991e9e8e4788da60c66057f73d2dc8d9d
janac meena <janac.meena@gmail.com>
2016-04-08 18:20:20 -0400

    fixed interval bug

--- startup.js
+++ startup.js
@@ -4,46 +4,46 @@ var apiKey = 'AIzaSyBZn5Aizlu94stQI6oCdz4SpwIqHfhyfQo';
 
 // from https://developers.google.com/api-client-library/javascript/start/start-js#how-it-looks-in-javascript
 function handleClientLoad() {
-    // Step 2: Reference the API key
-    gapi.client.setApiKey(apiKey);
-    window.setTimeout(checkAuth,1);
+// Step 2: Reference the API key
+gapi.client.setApiKey(apiKey);
+window.setTimeout(checkAuth,1);
 }
 
 /**
- * Check if current user has authorized this application.
- */
+* Check if current user has authorized this application.
+*/
 function checkAuth() {
     gapi.auth.authorize(
-        {
-            'client_id': CLIENT_ID,
-            'scope': SCOPES.join(' '),
-            'immediate': true
-        }, handleAuthResult);
+    {
+        'client_id': CLIENT_ID,
+        'scope': SCOPES.join(' '),
+        'immediate': true
+    }, handleAuthResult);
 }
 
 /**
- * Handle response from authorization server.
- *
- * @param {Object} authResult Authorization result.
- */
+* Handle response from authorization server.
+*
+* @param {Object} authResult Authorization result.
+*/
 function handleAuthResult(authResult) {
     var authorizeDiv = document.getElementById('authorize-div');
     if (authResult && !authResult.error) {
-        // Hide auth UI, then load client library.
-        authorizeDiv.style.display = 'none';
-        // loadCalendarApi();
-    } else {
-        // Show auth UI, allowing the user to initiate authorization by
-        // clicking authorize button.
-        authorizeDiv.style.display = 'inline';
-    }
+// Hide auth UI, then load client library.
+authorizeDiv.style.display = 'none';
+// loadCalendarApi();
+} else {
+// Show auth UI, allowing the user to initiate authorization by
+// clicking authorize button.
+authorizeDiv.style.display = 'inline';
+}
 }
 
 /**
- * Initiate auth flow in response to user clicking authorize button.
- *
- * @param {Event} event Button click event.
- */
+* Initiate auth flow in response to user clicking authorize button.
+*
+* @param {Event} event Button click event.
+*/
 function handleAuthClick(event) {
     gapi.auth.authorize(
         {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
@@ -52,52 +52,27 @@ function handleAuthClick(event) {
 }
 
 /**
- * Load Google Calendar client library. List upcoming events
- * once client library is loaded.
- * J: looks like this is alternative to using REST or CORS.
- */
+* Load Google Calendar client library. List upcoming events
+* once client library is loaded.
+* J: looks like this is alternative to using REST or CORS.
+*/
 //J: perhaps here, i would change it to "addEventEntry" so that on load it runs that function
 // but how do i make it load multiple times?
+function getIntervalTime() {
+    var i = 1800 * 1000;  //half hr
+    // var i = 10000;
+    return i;
+}
+
 function loadCalendarApi() {
-    // gapi.client.load('calendar', 'v3', setInterval(createEvent,2000)); // with interval popups
-    gapi.client.load('calendar', 'v3', createEvent);
+    var hh = getIntervalTime();
+    console.log("HH IS: " + hh);
+// gapi.client.load('calendar', 'v3', setInterval(createEvent,parseInt(hh)));
+gapi.client.load('calendar', 'v3', setInterval(createEvent,getIntervalTime())); // with interval popups
+// gapi.client.load('calendar', 'v3', setInterval(createEvent,10000)); // with interval popups
+// gapi.client.load('calendar', 'v3', setInterval(createEvent,2000)); // with interval popups
+// gapi.client.load('calendar', 'v3', createEvent);
 }
-
-/**
- * Print the summary and start datetime/date of the next ten events in
- * the authorized user's calendar. If no events are found an
- * appropriate message is printed.
- */
-function listUpcomingEvents() {
-    // var request = gapi.client.calendar.events.list({
-    //   'calendarId': 'primary',
-    //   'timeMin': (new Date()).toISOString(),
-    //   'showDeleted': false,
-    //   'singleEvents': true,
-    //   'maxResults': 10,
-    //   'orderBy': 'startTime'
-    // });
-
-    // request.execute(function(resp) {
-    //   var events = resp.items;
-    //   appendPre('Upcoming events:');
-
-    //   if (events.length > 0) {
-    //     for (i = 0; i < events.length; i++) {
-    //       var event = events[i];
-    //       var when = event.start.dateTime;
-    //       if (!when) {
-    //         when = event.start.date;
-    //       }
-    //       appendPre(event.summary + ' (' + when + ')')
-    //     }
-    //   } else {
-    //     appendPre('No upcoming events found.');
-    //   }
-
-    // });
-}
-
 // code below is from https://developers.google.com/google-apps/calendar/v3/reference/events/insert#examples
 var countOfprompts= 0;
 function createEvent() {
@@ -111,43 +86,43 @@ function createEvent() {
         'summary': userInput,
         'description': 'Created by reverse calendar',
         'start': {
-            // 'dateTime': '2016-03-14T09:00:00-04:00', //the time specified here will be the time in toronto
-            'dateTime': new Date(Date.now() - 3600*1000),
-            'timeZone': 'America/Toronto'
-        },
+        // 'dateTime': '2016-03-14T09:00:00-04:00', //the time specified here will be the time in toronto
+        // 'dateTime': new Date(Date.now() - 3600*1000),
+        'dateTime': new Date(Date.now() - getIntervalTime()),
+        'timeZone': 'America/Toronto'
+    },
         'end': {
-            // 'dateTime': '2017-03-14T10:00:00-04:00',
-            'dateTime': currentTime, //should round this to nearest half-hour?
-            'timeZone': 'America/Toronto'
-        },
-        'reminders': {
-            'useDefault': false,
-            'overrides': [
-                {'method': 'email', 'minutes': 24 * 60},
-                {'method': 'popup', 'minutes': 10}
-            ]
-        }
-    };
+        // 'dateTime': '2017-03-14T10:00:00-04:00',
+        'dateTime': currentTime, //should round this to nearest half-hour?
+        'timeZone': 'America/Toronto'
+    },
+    'reminders': {
+        'useDefault': false,
+        'overrides': [
+            {'method': 'email', 'minutes': 24 * 60},
+            {'method': 'popup', 'minutes': 10}
+        ]
+    }
+};
 
-    var request = gapi.client.calendar.events.insert({ //assemble the request
-        'calendarId': 'primary',
-        'resource': event
-    });
+var request = gapi.client.calendar.events.insert({ //assemble the request
+    'calendarId': 'primary',
+    'resource': event
+});
 
-    request.execute(function(event) { //execute the api request. could use "then" instead
-        appendPre('Event created: ' + event.htmlLink);
-    });
+request.execute(function(event) { //execute the api request. could use "then" instead
+    appendPre('Event created: ' + event.htmlLink);
+});
 }
 
 /**
- * Append a pre element to the body containing the given message
- * as its text node.
- *
- * @param {string} message Text to be placed in pre element.
- */
+* Append a pre element to the body containing the given message
+* as its text node.
+*
+* @param {string} message Text to be placed in pre element.
+*/
 function appendPre(message) {
     var pre = document.getElementById('output');
     var textContent = document.createTextNode(message + '\n');
     pre.appendChild(textContent);
 }
-
